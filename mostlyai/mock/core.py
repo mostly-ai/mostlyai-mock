c60a5a82a8ac461f2b805a738ad2a1a280b8143d
Lukasz Kolodziejczyk <lukasz.kolodziejczyk@mostly.ai>
2025-05-21 16:37:00 +0200

    fix

--- mostlyai/mock/core.py
+++ mostlyai/mock/core.py
@@ -266,7 +266,7 @@ def _create_table_prompt(
         prompt += f"## Context Table Primary Key: `{primary_keys[fk.referenced_table]}`\n\n"
 
         prompt += f"## Context Table Data:\n\n"
-        prompt += f"{context_data.to_json(orient='records', indent=2)}\n\n"
+        prompt += f"{context_data.to_json(orient='records', date_format='iso', indent=2)}\n\n"
 
     # add non-context table names, primary keys and data
     if foreign_keys and len(foreign_keys) > 1:
@@ -280,7 +280,7 @@ def _create_table_prompt(
             prompt += f"## Non-Context Table Primary Key: `{primary_keys[fk.referenced_table]}`\n\n"
 
             prompt += f"## Non-Context Table Data:\n\n"
-            prompt += f"{non_context_data[fk.referenced_table].to_json(orient='records', indent=2)}\n\n"
+            prompt += f"{non_context_data[fk.referenced_table].to_json(orient='records', date_format='iso', indent=2)}\n\n"
 
     # add instructions
     prompt += "\n## Instructions:\n\n"
