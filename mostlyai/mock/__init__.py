ec6dd09717d8898314e496dcf3e570ccf39e6c55
mostlyai-ci <ci@mostly.ai>
2025-10-15 16:09:00 +0000

    Bump version to 0.2.0

--- mostlyai/mock/__init__.py
+++ mostlyai/mock/__init__.py
@@ -15,4 +15,4 @@
 from mostlyai.mock.core import sample
 
 __all__ = ["sample"]
-__version__ = "0.1.18"  # Do not set this manually. Use poetry version [params].
+__version__ = "0.2.0"  # Do not set this manually. Use poetry version [params].
